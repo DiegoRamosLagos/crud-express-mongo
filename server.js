80ba6aaabd754cb19a1af499315679f894f7985a
randir <diegoa.ramosl@gmail.com>
2021-08-14 13:01:54 -0400

    feat: basic mongo tasks for get and post

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+username = 'root'
+password = 'root'
+connectionString = `mongodb+srv://${username}:${password}@cluster0.lhms6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
-})
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client =>{
+        console.log('Connected to the database')
+        const db = client.db('star-wars-quotes')
+        const quotesCollection = db.collection('quotes')
 
-app.get('/', ((req, res) => {
-    res.sendFile(__dirname + '/index.html')
-}))
+        app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
+        app.listen(3000, () => {
+            console.log('listening on port 3000')
+        })
+
+        app.get('/', ((req, res) => {
+            db.collection('quotes').find().toArray()
+                .then(results => {
+                    console.log(results)
+                })
+                .catch(error => console.error(error))
+            res.sendFile(__dirname + '/index.html')
+        }))
+
+        app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+                .then(() => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+    })
